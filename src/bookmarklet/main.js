34a7b405bb441513fc5c304a18a25c0d053fa38c
Oscar Dub <oscar.dub@import.io>
2015-02-16 08:15:27 +0000

    Add bookmarklet source

---
+++ src/bookmarklet/main.js
@@ -0,0 +1,9 @@
+/*jshint strict:false, browser:true */
+(function bookmarklet() {
+  var url = window.location.href;
+  if (url.indexOf('https://magic.import.io') === 0) {
+  	window.alert('magic.import.io is already loaded!');
+  	return false;
+  }
+  window.open('https://magic.import.io/?site='+encodeURIComponent(url), '_blank');
+}());
