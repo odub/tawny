def0a69d2c959afb2291c3f4b28da1a98f7c4906
Oscar Dub <oscardub@gmail.com>
2015-02-23 16:25:53 +0000

    Update scripts/main.js Remove window.onload Simplify banner activation code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,11 @@
 /*jshint strict:false, browser:true */
 
-window.onload = function(){
-  var bookmarkletBanner = document.getElementById('bookmarklet-explain');
-  var bookmarkletButton = document.getElementById('bookmarklet-button');
+var bookmarkletBanner = document.getElementById('bookmarklet-explain');
+var bookmarkletButton = document.getElementById('bookmarklet-button');
 
-  bookmarkletButton.addEventListener('mouseover', function() {
-    bookmarkletBanner.className += 'active';
-  });
-  bookmarkletButton.addEventListener('mouseout', function() {
-    bookmarkletBanner.className = bookmarkletBanner.className.replace( /(?:^|\s)active(?!\S)/g , '' );
-  });	
-};
+bookmarkletButton.addEventListener('mouseover', function() {
+  bookmarkletBanner.className = 'active';
+});
+bookmarkletButton.addEventListener('mouseout', function() {
+  bookmarkletBanner.className = '';
+});	
