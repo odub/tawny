e5326a553397d792697232d70ee060fba5b76410
Oscar Dub <oscardub@gmail.com>
2015-02-24 16:39:45 +0000

    Update bookmarklet to handle being loaded on the install page

--- src/bookmarklet/main.js
+++ src/bookmarklet/main.js
@@ -5,5 +5,9 @@
   	window.alert('magic.import.io is already loaded!');
   	return false;
   }
+  if (url.indexOf('http://odub.github.io/tawny/') === 0) {
+    window.alert('Browse to a target page to use with magic.import.io and try again!');
+    return false;
+  }
   window.open('https://magic.import.io/?site='+encodeURIComponent(url)+'&utm_source=bm&utm_medium=bm&utm_campaign=bm', '_blank');
 }());
